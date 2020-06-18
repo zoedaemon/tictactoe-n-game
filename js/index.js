9c8ab1af7a702e1749ae704960e3aee774e4d1c6
zoedaemon <lindroy34@gmail.com>
2020-06-19 02:54:19 +0700

    success implement click animation transition; TODO:need per node lock

--- js/index.js
+++ js/index.js
@@ -4,10 +4,26 @@ function clickBox() {
   alert("clicked");
 }
 
+var locked = false;
+
 /**changeShape: change to respective shape */
 function changeShape(obj, defaultShape, wantShape) {
+  if (locked == true) {
+    console.log("shaped has been locked");
+    return;
+  }
   console.log("change shaped");
   var regExp = new RegExp("(?:^|\\s)" + defaultShape + "(?!\\S)", "g");
   console.log(regExp);
   obj.className = obj.className.replace(regExp, wantShape);
+}
+
+function lockShape(obj, defaultShape, wantShape) {
+  console.log("lock shaped");
+  changeShape(obj, defaultShape, wantShape);
+  var delayInMilliseconds = 1000; //1 second
+  setTimeout(function () {
+    changeShape(obj, wantShape, defaultShape);
+    locked = true;
+  }, delayInMilliseconds);
 }
