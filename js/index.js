7743b959cfb8e7c6a31bb87ee226a699234757cc
zoedaemon <lindroy34@gmail.com>
2020-06-19 01:49:57 +0700

    success implement function js for change shape on hover; TODO: need mouseenter and mouseleave

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
+/**clickBox: just simple click */
 function clickBox() {
   console.log("clicked");
   alert("clicked");
+}
+
+/**changeShape: change to respective shape */
+function changeShape(obj, defaultShape, wantShape) {
+  console.log("change shaped");
+  var regExp = new RegExp("(?:^|\\s)" + defaultShape + "(?!\\S)", "g");
+  console.log(regExp);
+  obj.className = obj.className.replace(regExp, wantShape);
 }
